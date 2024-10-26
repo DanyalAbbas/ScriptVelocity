b642ebdb2414f1797685407ad73996a84917a539
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-26 17:25:31 +0500

    fix alignment issue

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 int main()
 {
 
-    printf("  SSSSS   CCCCC  RRRRR   III   PPPPPP  TTTTT   V     V  EEEEEEE   L         OOO    CCCCC  III   TTTTT   Y      Y\n");
+    printf("  SSSSS   CCCCC  RRRRR   III   PPPPPP  TTTTT   V     V  EEEEEEE   L         OOO    CCCCC  III   TTTTT   Y       Y\n");
     printf(" S       C       R    R   I    P    P    T      V   V   E         L        O   O  C        I      T       Y   Y \n");
     printf("  SSSS   C       RRRRR    I    PPPPPP    T       V V    EEEE      L        O   O  C        I      T         Y   \n");
     printf("      S  C       R   R    I    P         T        V     E         L        O   O  C        I      T         Y   \n");
