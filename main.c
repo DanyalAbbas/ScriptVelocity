f083fb9f4cbe15d491625b9c8295334a05c012b3
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-26 17:25:05 +0500

    add title

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <windows.h>
 #include <string.h>
 #include <time.h>
+#include <stdio.h>
 
+int main()
 {
-    
+
+    printf("  SSSSS   CCCCC  RRRRR   III   PPPPPP  TTTTT   V     V  EEEEEEE   L         OOO    CCCCC  III   TTTTT   Y      Y\n");
+    printf(" S       C       R    R   I    P    P    T      V   V   E         L        O   O  C        I      T       Y   Y \n");
+    printf("  SSSS   C       RRRRR    I    PPPPPP    T       V V    EEEE      L        O   O  C        I      T         Y   \n");
+    printf("      S  C       R   R    I    P         T        V     E         L        O   O  C        I      T         Y   \n");
+    printf(" SSSSS    CCCCC  R    R  III   P         T        V     EEEEEEE   LLLLLL    OOO    CCCCC  III     T         Y   \n");
+
+
+    return 0;
+
+
 }
